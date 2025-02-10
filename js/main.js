9579d54c996d6a6f2db51d11f9637f860cb9cfb4
zeroxx1-droid <ns73782900000@gmail.com>
2025-02-10 08:44:05 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('THIS IS FOR UHH').split('')
+    const titles = ('I ❤️ U').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
